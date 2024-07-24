6b7ad1ec1993f5aafd1206768c78cd8e82480a7a
Gontzal Aparicio <gontzal.aparicio@centum.com>
2024-07-24 16:36:07 +0200

    [BounceY'All]: Rhythm manager first processing iteration

--- scripts/utils/rhythm_manager.gd
+++ scripts/utils/rhythm_manager.gd
@@ -1,15 +1,24 @@
 extends Node2D
 
-@export var audio_player:AudioStreamPlayer2D
+@export var main_audio_player:AudioStreamPlayer
+@export var rhythm_player:AudioStreamPlayer
 @export var audio_bpm:int = 0
+@export var rhythm_timer:Timer
 var audio_check_freq:float = 0
+var time_since_last_check:float = 0
 const SECS_IN_MIN = 60.0
 
 # Called when any audio bpm change has been made. Not usable for now
 # but maybe in the future we want to change bpms on the go
 func _manage_freq_change():
 	if audio_bpm >= 0:
-		audio_check_freq = audio_bpm / SECS_IN_MIN
+		rhythm_timer.stop()
+		main_audio_player.stop()
+		audio_check_freq = SECS_IN_MIN / audio_bpm
+		print("Audio freq changed: "+ str(audio_check_freq))
+		rhythm_timer.wait_time = audio_check_freq
+		rhythm_timer.start()
+		main_audio_player.play()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,4 +26,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	time_since_last_check += delta
+	print(time_since_last_check)
+	if Input.is_action_just_pressed("space"):
+		rhythm_player.play()
+		_check_players_rhythm()
+
+func _check_players_rhythm():
+	if time_since_last_check <= audio_bpm:
+		print("Nice rhythm")
+
+# Called everytime the rhythm has to be checked
+func _on_rhythm_timer_timeout():
+	time_since_last_check = 0
